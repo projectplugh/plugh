4c10b54f8740aca776186e9937b8d46a79382d9e
David Pollak <feeder.of.the.bears@gmail.com>
2013-05-26 08:32:55 -0700

    Moved to aleph for dealing with http

--- project.clj
+++ project.clj
@@ -3,8 +3,10 @@
   :url "http://plugh.im"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [io.netty/netty "3.6.5.Final"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.novemberain/welle "1.5.0"]
+                 [aleph "0.3.0-rc1"]
+                 [org.clojure/core.match "0.2.0-alpha12"]
+                 ]
 
-  :aot [plugh.http]
   :main plugh.core)
